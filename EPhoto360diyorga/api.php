53f1bd311e249f1c94d7eb698f365f5ad32eb7de
arthurnnnn <79204485+arthurnnnn@users.noreply.github.com>
2021-04-22 21:26:58 -0300

    Update api.php

--- EPhoto360diyorga/api.php
+++ EPhoto360diyorga/api.php
@@ -3,6 +3,6 @@ error_reporting(0);
 header('Content-type: image/jpeg');
 $text = $_GET['text']; 
 $type = $_GET['type'];
-$get = file_get_contents("https://DOMEN.xvest.ru/PAPKANOMI/?act=writeText&output=image&effect=https://en.ephoto360.com/paint-splatter-text-effect-$type.html&text=$text");
+$get = file_get_contents("https://en.ephoto360.com/write-text-on-wet-glass-online-589.html.html&text=");
 echo $get;
 ?>
